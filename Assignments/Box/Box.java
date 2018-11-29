e58cc4a2bf9a63e60397454587d4823a9d1c4f7f
Anne Ouyang <anne.ouyang@gmail.com>
2018-11-29 09:29:16 -0800

    delete file

--- Assignments/Box/Box.java
+++ Assignments/Box/Box.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.awt.event.*;
 
 public class Box extends JComponent {
 
@@ -16,5 +17,32 @@ public class Box extends JComponent {
 		Graphics2D g2 = (Graphics2D) g;
 		Rectangle rect = new Rectangle(x, y, 100, 100);
 		g2.draw(rect);
+	}
+
+	public static void main(String args[]) {
+		JFrame frame = new JFrame();
+		frame.setSize(1000, 1000);
+		frame.setTitle("Box");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Box myBox = new Box(); // JComponent object
+
+		class MouseClick implements MouseListener {
+			public void mousePressed(MouseEvent event) {};
+			public void mouseReleased(MouseEvent event) {};
+			public void mouseEntered(MouseEvent event) {};
+			public void mouseClicked(MouseEvent event) {
+				int x = event.getX();
+				int y = event.getY();
+				myBox.updateLocation(x, y);
+			}
+			public void mouseExited(MouseEvent event) {};
+		}
+
+		MouseListener mouse = new MouseClick();
+		myBox.addMouseListener(mouse);
+
+		frame.add(myBox);
+		frame.setVisible(true);
 	}
 }
